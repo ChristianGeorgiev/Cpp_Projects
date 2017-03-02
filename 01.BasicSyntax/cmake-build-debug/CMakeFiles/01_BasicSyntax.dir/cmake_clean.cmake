69dd0dd193b170598600ae3b2d7c193963355ec3
ChristianGeorgiev <kristian.v.georgiev@gmail.com>
2017-03-02 17:19:29 +0200

    Initial Cpp Project

---
+++ 01.BasicSyntax/cmake-build-debug/CMakeFiles/01_BasicSyntax.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/01_BasicSyntax.dir/main.cpp.o"
+  "01_BasicSyntax.pdb"
+  "01_BasicSyntax"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_BasicSyntax.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
